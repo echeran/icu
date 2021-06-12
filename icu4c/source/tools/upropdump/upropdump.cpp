74aa2925b2d61a78f022c2960a5b199f3d44111d
Shane F. Carr <shane@unicode.org>
2021-06-12 01:45:31 -0500

    Change to upropdump

--- icu4c/source/tools/upropdump/upropdump.cpp
+++ icu4c/source/tools/upropdump/upropdump.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    std::cout << "hello from propdump" << std::endl;
+    std::cout << "hello from upropdump" << std::endl;
     for (int i=0; i<argc; i++) {
         std::cout << "arg" << i << ": " << argv[i] << std::endl;
     }
