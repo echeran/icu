84dd483e7c470f346dfad79997f41a33bb67bc22
Shane F. Carr <shane@unicode.org>
2021-06-12 01:42:41 -0500

    ICU-21545 Add boilerplate for new propdump tool

---
+++ icu4c/source/tools/propdump/propdump.cpp
@@ -0,0 +1,11 @@
+// © 2016 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "hello from propdump" << std::endl;
+    for (int i=0; i<argc; i++) {
+        std::cout << "arg" << i << ": " << argv[i] << std::endl;
+    }
+}
