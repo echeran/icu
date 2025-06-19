303b74f08d81713647ea543cccd258eabbdac128
Elango Cheran <elango@unicode.org>
2025-06-18 21:15:00 -0700

    ICU-22789 Simplify code by deduplicating via helper impl class

--- icu4j/main/core/src/main/java/com/ibm/icu/segmenter/LocalizedSegmenter.java
+++ icu4j/main/core/src/main/java/com/ibm/icu/segmenter/LocalizedSegmenter.java
@@ -28,7 +28,7 @@ public class LocalizedSegmenter implements Segmenter {
    */
   @Override
   public Segments segment(CharSequence s) {
-    return new LocalizedSegments(this, s);
+    return new SegmentsImpl(breakIterPrototype, s);
   }
 
   /**
@@ -54,10 +54,6 @@ public class LocalizedSegmenter implements Segmenter {
         breakIterPrototype = BreakIterator.getCharacterInstance(locale);
         break;
     }
-  }
-
-  private BreakIterator getNewBreakIterator() {
-    return (BreakIterator) breakIterPrototype.clone();
   }
 
   /**
@@ -136,124 +132,5 @@ public class LocalizedSegmenter implements Segmenter {
       return new LocalizedSegmenter(locale, segmentationType);
     }
 
-  }
-
-  private class LocalizedSegments implements Segments {
-
-    private CharSequence source;
-
-    private BreakIterator breakIterPrototype;
-
-    private LocalizedSegments(LocalizedSegmenter segmenter, CharSequence source) {
-      this.source = source;
-
-      // We are creating a clone of the Segmenter's prototype BreakIterator field so that this
-      // concrete Segments object can avoid sharing state with the other Segments object instances
-      // that get spawned from the Segmenter. This allows difference source CharSequences to be used
-      // in each Segments object.
-      //
-      // In turn, the cloned BreakIterator becomes a prototype to be stored in the Segments object,
-      // which then gets cloned and used in each of the Segments APIs' implementations. The second
-      // level of cloning that happens when the Segments object's local BreakIterator prototype
-      // gets cloned allows the iteration state to be separate whenever an Segments API is called.
-      // Otherwise, there is a chance that multiple API calls on the same Segments object might
-      // mutate the same position/index, if done concurrently.
-      breakIterPrototype = (BreakIterator) segmenter.getNewBreakIterator();
-      // It's okay to perform .setText on the object that we want to clone later because we should
-      // then not have to call .setText on the clones.
-      breakIterPrototype.setText(source);
-    }
-
-    /**
-     * Returns the segment that contains index {@code i}. Containment is inclusive of the start index
-     * and exclusive of the limit index.
-     * @param i index in the input {@code CharSequence} to the {@code Segmenter}
-     * @return A segment that either starts at or contains index {@code i}
-     * @draft ICU 78
-     */
-    @Override
-    public Segment segmentAt(int i) {
-      return SegmentsImplUtils.segmentAt((BreakIterator) breakIterPrototype.clone(), source, i);
-    }
-
-    /**
-     * Returns a {@code Stream} of all {@code Segment}s in the source sequence. Start with the first
-     * and iterate forwards until the end of the sequence.
-     * @return a {@code Stream} of all {@code Segments} in the source sequence.
-     * @draft ICU 78
-     */
-    @Override
-    public Stream<Segment> segments() {
-      return SegmentsImplUtils.segments((BreakIterator) breakIterPrototype.clone(), source);
-    }
-
-    /**
-     * Returns whether offset {@code i} is a segmentation boundary. Throws an exception when
-     * {@code i} is not a valid index position for the source sequence.
-     * @param i index in the input {@code CharSequence} to the {@code Segmenter}
-     * @return Returns whether offset {@code i} is a segmentation boundary.
-     * @draft ICU 78
-     */
-    @Override
-    public boolean isBoundary(int i) {
-      return ((BreakIterator) breakIterPrototype.clone()).isBoundary(i);
-    }
-
-    /**
-     * Returns a {@code Stream} of all {@code Segment}s in the source sequence where all segment limits
-     * {@code  l} satisfy {@code i < l}.  Iteration moves forwards.
-     * @param i index in the input {@code CharSequence} to the {@code Segmenter}
-     * @return a {@code Stream} of all {@code Segment}s at or after {@code i}
-     * @draft ICU 78
-     */
-    @Override
-    public Stream<Segment> segmentsFrom(int i) {
-      return SegmentsImplUtils.segmentsFrom((BreakIterator) breakIterPrototype.clone(), source, i);
-    }
-
-    /**
-     * Returns a {@code Stream} of all {@code Segment}s in the source sequence where all segment
-     * limits {@code  l} satisfy {@code l ≤ i}. Iteration moves backwards.
-     * @param i index in the input {@code CharSequence} to the {@code Segmenter}
-     * @return a {@code Stream} of all {@code Segment}s before {@code i}
-     * @draft ICU 78
-     */
-    @Override
-    public Stream<Segment> segmentsBefore(int i) {
-      return SegmentsImplUtils.segmentsBefore((BreakIterator) breakIterPrototype.clone(), source, i);
-    }
-
-    /**
-     * Returns all segmentation boundaries, starting from the beginning and moving forwards.
-     * @return An {@code IntStream} of all segmentation boundaries, starting at the first
-     * boundary with index 0, and moving forwards in the input sequence.
-     * @draft ICU 78
-     */
-    @Override
-    public IntStream boundaries() {
-      return SegmentsImplUtils.boundaries((BreakIterator) breakIterPrototype.clone(), source);
-    }
-
-    /**
-     * Returns all segmentation boundaries after the provided index.  Iteration moves forwards.
-     * @param i index in the input {@code CharSequence} to the {@code Segmenter}
-     * @return An {@code IntStream} of all boundaries {@code b} such that {@code b > i}
-     * @draft ICU 78
-     */
-    @Override
-    public IntStream boundariesAfter(int i) {
-      return SegmentsImplUtils.boundariesAfter((BreakIterator) breakIterPrototype.clone(), source, i);
-    }
-
-    /**
-     * Returns all segmentation boundaries on or before the provided index. Iteration moves backwards.
-     * @param i index in the input {@code CharSequence} to the {@code Segmenter}
-     * @return An {@code IntStream} of all boundaries {@code b} such that {@code b ≤ i}
-     * @draft ICU 78
-     */
-    @Override
-    public IntStream boundariesBackFrom(int i) {
-      return SegmentsImplUtils.boundariesBackFrom((BreakIterator) breakIterPrototype.clone(), source, i);
-    }
   }
 }
